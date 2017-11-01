0c112e42c34f3de60d5bc2021d82c9d26d197574
suvigya1st <suvigya1st@gmail.com>
2017-11-01 17:49:36 +0530

    Final working Code

--- priority.cu
+++ priority.cu
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#define THREADS 512 // 2^9
+#define THREADS 256 // 2^9
 #define BLOCKS 32 // 2^15
 #define NUM THREADS*BLOCKS
 
@@ -28,7 +28,7 @@ void print_array(int *arr1,int *arr2 ,int *arr3,int *arr4, int length)
   int i;
   for (i = 0; i < length; ++i) {
     //arr[i] = random_float();
-    printf("%d\t%d\t%d\t%d\n",arr1[i],arr2[i],arr3[i],arr4[i]);
+    printf("%d\t%d\t%d\t%d\t%d\n",i+1,arr1[i],arr2[i],arr3[i],arr4[i]);
   }
 }
 
@@ -130,17 +130,76 @@ __global__ void inclusive_scan(int *X, int *Y, int N)
   }
 }
 
+__global__ void work_inefficient_scan_kernel(int *X, int *Y, int InputSize)
+{
+  __shared__ int XY[THREADS];
+  int i= blockIdx.x*blockDim.x+ threadIdx.x;
+  {
+    XY[threadIdx.x] = X[i];
+  }
+  // the code below performs iterative scan on XY
+  for (unsigned int stride = 1; stride <= threadIdx.x; stride *= 2)
+  {
+    __syncthreads();
+    XY[threadIdx.x] += XY[threadIdx.x-stride];
+  }
+  Y[i] = XY[threadIdx.x];
+}
+
+__global__ void work_efficient_scan_kernel(int *X, int *Y, int InputSize)
+{
+  __shared__ int XY[THREADS];
+  int i= blockIdx.x*blockDim.x+ threadIdx.x;
+  if (i < InputSize)
+  {
+    XY[threadIdx.x] = X[i];
+  }
+  for (unsigned int stride = 1; stride < blockDim.x; stride *= 2)
+  {
+    __syncthreads();
+    int index = (threadIdx.x+1) * 2* stride -1;
+    if (index < blockDim.x)
+    {
+      XY[index] += XY[index -stride];
+    }
+  }
+  for (int stride = THREADS/4; stride > 0; stride /= 2)
+  {
+    __syncthreads();
+    int index = (threadIdx.x+1)*stride*2 -1;
+    if(index + stride < THREADS)
+    {
+     XY[index + stride] += XY[index];
+    }
+  }
+  __syncthreads();
+  Y[i] = XY[threadIdx.x];
+
+  //OWN CODE
+  __syncthreads();
+  if(threadIdx.x < blockIdx.x)
+  {
+    XY[threadIdx.x] = Y[threadIdx.x*blockDim.x + (blockDim.x-1)];
+  }
+  __syncthreads();
+  for(unsigned int stride =0; stride < blockIdx.x; stride++)
+  {
+    Y[i] += XY[stride];
+  }
+  __syncthreads();
+}
+
 void scan_next(int *bt, int *tat)
 {
   dim3 blocks(BLOCKS,1);
   dim3 threads(THREADS,1);
 
-  inclusive_scan<<<blocks, threads, THREADS * sizeof(int)>>>(bt, tat,NUM);
+  work_efficient_scan_kernel<<<blocks, threads, THREADS * sizeof(int)>>>(bt, tat,NUM);
 }
 
 int main()
 {
-	//int h_pid[NUM],h_wt[NUM],h_tat[NUM],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+  //int h_pid[NUM],h_wt[NUM],h_tat[NUM],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
   // printf("Enter Total Number of Process:");
   // scanf("%d",&n);
 
@@ -150,10 +209,10 @@ int main()
   int *h_pr = (int*) malloc( NUM * sizeof(int));
   int *h_tat = (int*) malloc( NUM * sizeof(int));
   int *h_wt = (int*) malloc( NUM * sizeof(int));
-	array_fill(h_bt, NUM);
-	array_fill(h_pr, NUM);
+  array_fill(h_bt, NUM);
+  array_fill(h_pr, NUM);
   printf("INITIAL\n");
-	printf("PR\tBT\tWT\tTAT\n");
+  printf("\tPR\tBT\tWT\tTAT\n");
   print_array(h_pr,h_bt,h_wt,h_tat,NUM);
 
   int *d_bt, *d_pr, *d_wt, *d_tat;
@@ -189,8 +248,8 @@ int main()
   stop = clock();
 
   printf("\nFINAL\n");
-  printf("PR\tBT\tWT\tTAT\n");
+  printf("\tPR\tBT\tWT\tTAT\n");
   print_array(h_pr,h_bt,h_wt,h_tat,NUM);
   print_elapsed(start, stop);
 
-}	
+} 
